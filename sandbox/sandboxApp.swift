c1433b2e5c7cabfc4addcc4eff5ffb6ac3f91d40
hsbaewa <hsbaewa@8snippet.com>
2024-07-02 10:57:12 +0900

    init

---
+++ sandbox/sandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandboxApp.swift
+//  sandbox
+//
+//  Created by hsbaewa on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct sandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
